8cc79c3f14c560698300906392af86e1d79e53c6
PabloRNC <pabloruanonavarro@gmail.com>
2025-09-13 00:37:01 +0200

    fix(): Prevent creating request objects per get

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -9,43 +9,43 @@ import type { EventsResponse } from '../interfaces/EventsResponse'
 import type { Brawlers } from '../utils/Brawlers'
 
 export class Client {
-	public token: string | undefined
+	public requestManager: Request
 	constructor(token: string | undefined) {
-		this.token = token
+		this.requestManager = new Request(token);
 	}
 
 	public async getPlayer(tag: string | undefined, battlelog = false): Promise<Player> {
-		return new Player(await new Request(this.token).getPlayer(tag, battlelog), this)
+		return new Player(await this.requestManager.getPlayer(tag, battlelog), this)
 	}
 	public async getBattleLog(tag: string | undefined): Promise<BattleLog> {
-		return new BattleLog(await new Request(this.token).getBattleLog(tag))
+		return new BattleLog(await this.requestManager.getBattleLog(tag))
 	}
 	public async getClub(tag: string | undefined): Promise<Club> {
-		return new Club(await new Request(this.token).getClub(tag), this)
+		return new Club(await this.requestManager.getClub(tag), this)
 	}
 	public async getBrawler(brawler: Brawlers | undefined): Promise<Brawler> {
-		return new Brawler(await new Request(this.token).getBrawler(brawler))
+		return new Brawler(await this.requestManager.getBrawler(brawler))
 	}
 	public async getBrawlers(): Promise<Brawler[]> {
-		return await new Request(this.token).getBrawlers()
+		return await this.requestManager.getBrawlers()
 	}
 	public async getRankingOfPlayers(
 		countryCode = 'global'
 	): Promise<RankingOfPlayersResponse[]> {
-		return await new Request(this.token).getRankingOfPlayers(countryCode)
+		return await this.requestManager.getRankingOfPlayers(countryCode)
 	}
 	public async getRankingOfClubs(
 		countryCode = 'global'
 	): Promise<RankingOfClubsResponse[]> {
-		return await new Request(this.token).getRankingOfClubs(countryCode)
+		return await this.requestManager.getRankingOfClubs(countryCode)
 	}
 	public async getRankingOfBrawlers(
 		brawler: Brawlers | undefined,
 		countryCode = 'global'
 	): Promise<RankingOfPlayersResponse[]> {
-		return await new Request(this.token).getRankingOfBrawlers(countryCode, brawler)
+		return await this.requestManager.getRankingOfBrawlers(countryCode, brawler)
 	}
 	public async getEvents(): Promise<EventsResponse[]> {
-		return await new Request(this.token).getEvents()
+		return await this.requestManager.getEvents()
 	}
 }
